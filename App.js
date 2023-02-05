3f527872398d9858dc250a1d60aa7586d3c22187
mcalvi84 <86073164+mcalvi84@users.noreply.github.com>
2023-02-05 17:21:14 -0300

    Vistas y Navegacion

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import ShopNavigator from './src/navigation/ShopNavigator';
+
+export default function App() {
+  const [fontsLoaded] = useFonts ({
+    DancingScript: require("./src/assets/fonts/DancingScript-Regular.ttf"),
+  })
+
+if(!fontsLoaded){
+  return null
+}
+
+  return (
+   <ShopNavigator />
+
+  );
+}
+
+
